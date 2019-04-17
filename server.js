a90c233503c26f1b486e7c3f047e92a34c9d3a5f
niteshjain261992 <niteshjain261992@gmail.com>
2019-04-17 15:59:02 +0530

    changes of new version of prerender

---
+++ server.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+var prerender = require('./lib');
+
+var server = prerender();
+
+server.use(prerender.sendPrerenderHeader());
+// server.use(prerender.blockResources());
+server.use(prerender.removeScriptTags());
+server.use(prerender.httpHeaders());
+server.use(prerender.s3fsCache());
+
+server.start();
